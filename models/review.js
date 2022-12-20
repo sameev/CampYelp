d54c389e0430052ec2f75dd6715cdf3ea9a85e0c
Samee Vohra <samee.vohra@gmail.com>
2022-12-19 19:21:05 -0800

    review styling and title addition

--- models/review.js
+++ models/review.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
+  title: String,
   body: String,
   rating: Number
 })
--- schemas.js
+++ schemas.js
@@ -12,6 +12,7 @@ module.exports.campgroundSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
+    title: Joi.string().required(),
     rating: Joi.number().required().min(1).max(5),
     body: Joi.string().required(),
   }).required(),
