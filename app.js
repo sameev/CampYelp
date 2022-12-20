ea41f9304a725e150355b27b86976504dd01887c
Samee Vohra <samee.vohra@gmail.com>
2022-12-19 18:53:15 -0800

    display reviews on show pages

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.get('/campgrounds/new', (req, res) => {
 app.get(
   '/campgrounds/:id',
   asyncErrorWrapper(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
   })
 );
