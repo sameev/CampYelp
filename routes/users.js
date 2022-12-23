afd9292dea56881406b00a02a0048547b45203f4
Samee Vohra <samee.vohra@gmail.com>
2022-12-23 14:40:13 -0800

    updating register route to invoke login

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,18 @@ router.get('/register', (req, res) => {
 
 router.post(
   '/register',
-  asyncErrorWrapper(async (req, res) => {
+  asyncErrorWrapper(async (req, res, next) => {
     try {
       const { username, email, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash('success', `Welcome to CampersYelp, ${user.username}!`);
-      res.redirect('/campgrounds');
+      
+      req.login(registeredUser, (err) => {
+        if(err) return next(err);
+        req.flash('success', `Welcome to CampersYelp, ${user.username}!`);
+        res.redirect('/campgrounds');
+      })
+      
     } catch (e) {
       req.flash('error', e.message);
       res.redirect('/register');
