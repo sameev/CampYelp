2c0fced61512ee35360f0bf23eecb5f21082868b
Samee Vohra <samee.vohra@gmail.com>
2022-12-30 21:04:29 -0800

    added mapbox marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,4 +7,11 @@ const map = new mapboxgl.Map({
   zoom: 9, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({
+      offset: 25,
+    }).setHTML(`<h5>${campground.title}</h5> <p>${campground.location}</p>`)
+  )
+  .addTo(map);
