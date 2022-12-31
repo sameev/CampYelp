43f95d3c860e352e81c76f6a7bab117167d4efee
Samee Vohra <samee.vohra@gmail.com>
2022-12-30 17:09:32 -0800

    delete image from mongo/cloudinary functionality

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -44,6 +45,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
+  console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
@@ -54,6 +56,24 @@ module.exports.updateCampground = async (req, res) => {
   }));
 
   campground.images.push(...imgs);
+
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+
+    await campground.updateOne({
+      $pull: {
+        images: {
+          filename: {
+            $in: req.body.deleteImages,
+          },
+        },
+      },
+    });
+    console.log(campground);
+  }
+
   await campground.save();
 
   req.flash('success', `${campground.title} was successfully updated`);
